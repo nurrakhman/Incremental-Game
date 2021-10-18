c2c6c7fadcff028130f7ff021b593841d65c104d
nurrakhman <nurrakhman.gincode@gmail.com>
2021-10-18 13:53:43 +0700

    Add Game Manager Script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance = null;
+    public static GameManager instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    [Range(0f, 1f)]
+    public float AutoCollectPercentage = 0.1f;
+    public ResourceConfig[] ResourcesConfigs;
+
+    public Transform ResourcesParent;
+    public ResourceController ResourcePrefab;
+
+    public Text GoldInfo;
+    public Text AutoCollectInfo;
+
+    private List<ResourceController> _activeResources = new List<ResourceController>();
+    private float _collectSecond;
+
+    private double _totalGold;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddAllResources();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _collectSecond += Time.unscaledDeltaTime;
+        if(_collectSecond >= 1f)
+        {
+            CollectPerSecond();
+            _collectSecond = 0f;
+        }
+    }
+
+    private void AddAllResources()
+    {
+        foreach (ResourceConfig config in ResourcesConfigs)
+
+        {
+
+            GameObject obj = Instantiate(ResourcePrefab.gameObject, ResourcesParent, false);
+
+            ResourceController resource = obj.GetComponent<ResourceController>();
+
+
+
+            resource.SetConfig(config);
+
+            _activeResources.Add(resource);
+
+        }
+    }
+
+    private void CollectPerSecond()
+
+    {
+
+        double output = 0;
+
+        foreach (ResourceController resource in _activeResources)
+
+        {
+
+            output += resource.GetOutput();
+
+        }
+
+
+
+        output *= AutoCollectPercentage;
+
+        // Fungsi ToString("F1") ialah membulatkan angka menjadi desimal yang memiliki 1 angka di belakang koma 
+
+        AutoCollectInfo.text = $"Auto Collect: { output.ToString("F1") } / second";
+
+
+
+        AddGold(output);
+
+    }
+
+
+
+    private void AddGold(double value)
+
+    {
+
+        _totalGold += value;
+
+        GoldInfo.text = $"Gold: { _totalGold.ToString("0") }";
+
     }
 
 }
